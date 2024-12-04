290bad667b33eb5abcacb168276ba54a151d16b1
noosrat-jahan <noosratmeem@gmail.com>
2024-12-04 22:34:54 +0600

    Assignment-10 a crowd funding application or website named SparkFund and the client site environment setup is done here.

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const Router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1>I am a website</h1>
+    }
+])
+
+export default Router;
